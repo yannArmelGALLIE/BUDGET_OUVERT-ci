64044ec08e98fcf7bb6fbf2de82f430c10d371aa
GALLIE Koffi Yann-Armel <professional.gallie@gmail.com>
2026-05-02 19:23:05 +0000

    Connection à Hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,17 @@
+import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+  },
+};
+
+export default config;
